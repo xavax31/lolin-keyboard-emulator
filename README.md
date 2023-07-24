683e885b286982b0df2b4e9fa0341cd2dc0de18f
xab <xavier.boisnon@gmail.com>
2023-07-24 20:16:27 +0200

    update doc

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Upload script
 I needed to install:  
 
 - 'BleKeyboard.h': see <https://github.com/T-vK/ESP32-BLE-Keyboard>  
-- 'Keypad':  installed by Arduino IDE (tools | manage libraries | Keypad by Mark Stanley) 
+- 'Keypad':  installed by Arduino IDE (tools | manage libraries | Keypad by Mark Stanley)  
 
 An error occurs while compiling: Not found: ~/Documents/Arduino/hardware/espressif/esp32/tools/openocd-esp32/share/openocd/scripts/board/esp32-wrover-kit-3.3v.cfg  
 To resolve it, I needed to:  
@@ -30,3 +30,9 @@ Copy ~/Documents/Arduino/hardware/espressif/esp32/tools/openocd-esp32/tcl/board/
 
 see <https://demo-dijiudu.readthedocs.io/en/stable/api-guides/openocd.html#configuring-the-esp32-target-in-openocd> for more details
 
+/!\ The lolin must be alimented via usb or battery. Note that the usb is not used for keyboard emulation, only for alimentation (using usb for keyboard emulation need specifics chipsets (esp32-S2, arduino micro, ...). The lolin d32 pro CANNOT be used for keyboard emulation via USB)
+
+## Configuration
+
+For my own purpose, I send specifics keys, differents from original (using this to control page change with arrows keys). In this exemple, press on key '7' send an left arrow key, press on * send an right arrow key, and press on '0' send an down arrow key.  
+Adapt this as your own need (see BleKeyboard.h for all available keys).
--- lolin-keyboard-emulator.ino
+++ lolin-keyboard-emulator.ino
@@ -10,8 +10,11 @@
 
 #define VERSION "1.0.0"
 
-const byte ROWS = 4;
-const byte COLS = 4;
+// KEYPAD
+const byte ROWS = 4; // num of rows
+const byte COLS = 4; // num of columns
+
+// keys disposition
 char keys[ROWS][COLS] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
@@ -47,7 +50,40 @@ void loop()
     Serial.println(key);
     if (bleKeyboard.isConnected())
     {
-      if (key == '7')
+      // adapt here to send different keys values
+      if (key == '1')
+      {
+        bleKeyboard.write(KEY_NUM_1);
+      }
+      else if (key == '2')
+      {
+        bleKeyboard.write(KEY_NUM_2);
+      }
+      else if (key == '3')
+      {
+        bleKeyboard.write(KEY_NUM_3);
+      }
+      else if (key == '4')
+      {
+        bleKeyboard.write(KEY_NUM_4);
+      }
+      else if (key == '5')
+      {
+        bleKeyboard.write(KEY_NUM_5);
+      }
+      else if (key == '6')
+      {
+        bleKeyboard.write(KEY_NUM_6);
+      }
+      else if (key == '8')
+      {
+        bleKeyboard.write(KEY_NUM_8);
+      }
+      else if (key == '9')
+      {
+        bleKeyboard.write(KEY_NUM_9);
+      }
+      else if (key == '7')
       {
         bleKeyboard.write(KEY_LEFT_ARROW);
       }
@@ -58,6 +94,20 @@ void loop()
       else if (key == '0')
       {
         bleKeyboard.write(KEY_DOWN_ARROW);
+      }
+      else if (key == 'A')
+      {
+        bleKeyboard.write('Q');
+      }
+      else if (key == '#')
+      {
+        bleKeyboard.press(KEY_LEFT_SHIFT);
+        bleKeyboard.press('@');
+        bleKeyboard.releaseAll();
+      }
+      else
+      {
+        bleKeyboard.write(key);
       }
     }
   }
